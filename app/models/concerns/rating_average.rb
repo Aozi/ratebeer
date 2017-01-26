ef0465a0e2145f7e48c562c90b9690b672783747
Aozi <niko.novitsky@gmail.com>
2017-01-26 22:12:44 +0000

    V2 valmis

---
+++ app/models/concerns/rating_average.rb
@@ -0,0 +1,11 @@
+module Rating_Average
+  extend ActiveSupport::Concern
+
+  def average_rating
+    list = Array.new
+    ratings.each { |r| list.push(r.score)}
+    sum = list.inject { |sum, n| sum + n }
+    sum / ratings.count
+  end
+
+end
